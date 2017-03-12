cba23424afb947027d228173481cdeae92a58e03
Monica Beas <monica.j.beas@gmail.com>
2017-03-12 15:14:55 -0700

    added boostrap form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,72 @@
 class ProductsController < ApplicationController
 
-	def view_products 
+	def index 
 		@products = Product.all 
 
-		render 'view_products.html.erb'	
+		render 'index.html.erb'	
+	end 
+
+	def show 
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+
+	end 
+
+	def new 
+	end 
+
+	def create 
+		input_name = params[:name]
+		input_price = params[:price]
+		input_description = params[:description]
+
+		@product = Product.create(name: input_name, price: input_price, description: input_description)
+
+	end 
+		
+	def edit 
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+	end 
+
+	def update 
+		input_name = params[:name]
+		input_price = params[:price]
+		input_description = params[:description]
+
+		@product = Product.find_by(id: params[:id])
+		@product.name = input_name
+		@product.price = input_price
+		@product.description = input_description
+		@product.save 
+
+		redirect_to "/products/#{@product.id}"
+	end 
+
+	def destroy
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+		@product.destroy
+
+		redirect_to "/products"
+
+	end 
+
+	
+
+
+
+
+	def enter_product 
+
+	end 
+
+	def show_product 
+		input_name = params[:name]
+		input_price = params[:price]
+		input_description = params[:description]
+
+		@product = Product.create(name: input_name, price: input_price, description: input_description)
+
 	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get "/products" => "products#view_products"
 
+
+get "/enter_product" =>"products#enter_product"
+post "/show_product" => "products#show_product"
+
+#Index 
+get "/products" => "products#index"
+
+#New and Create
+get "/products/new" => "products#new"
+post "/products" => "products#create"
+
+# Show 
+get "/products/:id" => "products#show"
+
+# Edit and Update 
+get "/products/:id/edit" => "products#edit"
+patch "/products/:id" => "products#update"
+
+#Delete 
+delete "products/:id" => "products#destroy"
 
 end
